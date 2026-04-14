7606015d219b998566ad4bc8bec811fc46d21128
Elvie-Abarth <estibrusse18@gmail.com>
2026-04-14 09:57:41 +0200

    Implement CRUD operations for records in RecordsControllerv2

--- DR-Repo/Controllers/RecordsControllerv2.cs
+++ DR-Repo/Controllers/RecordsControllerv2.cs
@@ -21,4 +21,40 @@ public class RecordsControllerv2 : ControllerBase
         return Ok(records);
     }
 
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<Record> Create([FromBody] Record record)
+    {
+        var createdRecord = recordRepository.Create(record);
+        return CreatedAtAction(nameof(GetAll), new { id = createdRecord.Id }, createdRecord);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<Record> Delete([FromRoute] int id)
+    {
+        var record = recordRepository.GetById(id);
+        if (record == null)
+        {
+            return NotFound();
+        }
+
+        recordRepository.Delete(id);
+        return Ok(record);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<Record> Update([FromRoute] int id, [FromBody] Record record)
+    {
+        var existingRecord = recordRepository.GetById(id);
+        if (existingRecord == null)
+        {
+            return NotFound();
+        }
+
+        record.Id = id;
+        recordRepository.Update(record);
+        return Ok(record);
+    }
 }
